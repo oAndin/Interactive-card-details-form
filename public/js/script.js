bfe8a4c210be6609d42f5bce5ac312947f9f292d
oAndin <00andherson@gmail.com>
2023-07-23 01:46:55 -0300

    Update HTML, CSS and JS

--- public/js/script.js
+++ public/js/script.js
@@ -4,3 +4,27 @@ function verification() {
     event.preventDefault();
 }
 
+CVC.onfocus = function () {
+    cardInner.style.transform = `rotateY(180deg)`;
+}
+CVC.onblur = function () {
+    cardInner.style.transform = `rotateY(360deg)`;
+}
+
+// cardHolder.onFocus = function (){
+//     if (cardHolder.value != '') {}
+// }
+
+cardNumber.onkeyup = function (){
+    realNumber.innerHTML = `${cardNumber.value}`
+}
+cardHolder.onkeyup = function (){
+    realName.innerHTML = `${cardHolder.value}`
+}
+month.onkeyup = function (){
+    realDateMonth.innerHTML = `${month.value}`
+}
+year.onkeyup = function (){
+    realDateYear.innerHTML = `${year.value}`
+}
+
